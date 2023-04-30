8aecc4990378765bb9a858bd014729c56c46063a
Haikan lava <90133291+Haikanlava@users.noreply.github.com>
2023-04-30 12:48:34 +0300

    test 3

--- src/Utils.js
+++ src/Utils.js
@@ -2,5 +2,6 @@ export const fetchGoogleData = async (sheetName, setStateCallback) => {
   let response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1lPX9h_xT2eN5ed8lG5WdiiH4kphH4IRQlDLRU2qjft0/values/" + sheetName + "?key=AIzaSyABZeUkblAHJB7s8jRlea4jitjuqtff5k0")
   response = await (response.json());
   const finalData = response.values;
+  console.log(finalData);
   setStateCallback(finalData);
 }
