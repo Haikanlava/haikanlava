243acdae0fe05cf562e84eadc69dc06e25c500a6
Haikan lava <90133291+Haikanlava@users.noreply.github.com>
2023-04-30 13:08:09 +0300

    test 4

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,5 @@
 export const fetchGoogleData = async (sheetName, setStateCallback) => {
-  let response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1lPX9h_xT2eN5ed8lG5WdiiH4kphH4IRQlDLRU2qjft0/values/" + sheetName + "?key=AIzaSyABZeUkblAHJB7s8jRlea4jitjuqtff5k0")
+  let response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1lPX9h_xT2eN5ed8lG5WdiiH4kphH4IRQlDLRU2qjft0/values/" + sheetName + "?key=AIzaSyABZeUkblAHJB7s8jRlea4jitjuqtff5k0&valueRenderOption=FORMATTED_VALUE")
   response = await (response.json());
   const finalData = response.values;
   console.log(finalData);
